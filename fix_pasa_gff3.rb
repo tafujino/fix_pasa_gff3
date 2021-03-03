07a3fda47ff865641450067eebc3096f19ad17bf
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2021-03-03 17:53:51 +0900

    REFACTOR: remove unnecessary to_s

--- fix_pasa_gff3.rb
+++ fix_pasa_gff3.rb
@@ -13,14 +13,14 @@ def write_gene(gff3_entries)
   gene_mandatory_fields[Gff3Entry::START_COL_INDEX] = gff3_entries.first.start_pos
   gene_mandatory_fields[Gff3Entry::END_COL_INDEX] = gff3_entries.last.end_pos
   gene = gff3_entries.first.attributes[:ID]
-  puts Gff3Entry.new(gene_mandatory_fields, { ID: gene }).to_s
+  puts Gff3Entry.new(gene_mandatory_fields, { ID: gene })
   gff3_entries.each do |gff3|
     exon_mandatory_fields = gff3.mandatory_fields.clone
     exon_mandatory_fields[Gff3Entry::TYPE_COL_INDEX] = 'exon'
     exon_attributes = gff3.attributes
     exon_attributes.delete(:ID)
     exon_attributes[:Parent] = gene
-    puts Gff3Entry.new(exon_mandatory_fields, exon_attributes).to_s
+    puts Gff3Entry.new(exon_mandatory_fields, exon_attributes)
   end
   puts '###'
 end
