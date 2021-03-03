05343707baffa6b46de575729bb0e736bbe92a01
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2021-03-03 17:45:14 +0900

    FEAT: implement fix_pasa_gff3

---
+++ fix_pasa_gff3.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+require 'csv'
+
+require_relative 'gff3_entry'
+
+# @param gff3_entries [Array<Gff3Entry>] should be non-empty
+def write_gene(gff3_entries)
+  gene_mandatory_fields = gff3_entries.first.mandatory_fields.clone
+  gene_mandatory_fields[Gff3Entry::TYPE_COL_INDEX] = 'gene'
+  gene_mandatory_fields[Gff3Entry::START_COL_INDEX] = gff3_entries.first.start_pos
+  gene_mandatory_fields[Gff3Entry::END_COL_INDEX] = gff3_entries.last.end_pos
+  gene = gff3_entries.first.attributes[:ID]
+  puts Gff3Entry.new(gene_mandatory_fields, { ID: gene }).to_s
+  gff3_entries.each do |gff3|
+    exon_mandatory_fields = gff3.mandatory_fields.clone
+    exon_mandatory_fields[Gff3Entry::TYPE_COL_INDEX] = 'exon'
+    exon_attributes = gff3.attributes
+    exon_attributes.delete(:ID)
+    exon_attributes[:Parent] = gene
+    puts Gff3Entry.new(exon_mandatory_fields, exon_attributes).to_s
+  end
+  puts '###'
+end
+
+buffer = []
+puts '##gff-version 3'
+STDIN.each(chomp: true) do |line|
+  next if line =~ /^#/
+
+  gff3 = Gff3Entry.parse(line)
+  if buffer.empty? ||
+     buffer.last.attributes[:ID] == gff3.attributes[:ID]
+    buffer << gff3
+  else
+    write_gene(buffer)
+    buffer = [gff3]
+  end
+end
+
+write_gene(buffer) unless buffer.empty?
---
+++ gff3_entry.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal
+
+class Gff3Entry
+  TYPE_COL_INDEX = 2
+  START_COL_INDEX = 3
+  END_COL_INDEX = 4
+  ATTRIBULE_COL_INDEX = 8
+
+  # @return [Array<String>]
+  attr_reader :mandatory_fields
+
+  # @return [Hash { Symbol => String }]
+  attr_reader :attributes
+
+  # @param mandatory_fields [Array<String>]
+  # @param attributes       [Hash { Symbol => String }]
+  def initialize(mandatory_fields, attributes)
+    @mandatory_fields = mandatory_fields
+    @attributes = attributes
+  end
+
+  # @return [Integer]
+  def start_pos
+    @mandatory_fields[START_COL_INDEX].to_i
+  end
+
+  # @return [Integer]
+  def end_pos
+    @mandatory_fields[END_COL_INDEX].to_i
+  end
+
+  # @return [String]
+  def to_s
+    attr_field = @attributes.map { |k, v| "#{k}=#{v}" }.join(';')
+    (@mandatory_fields + [attr_field]).join("\t")
+  end
+
+  class << self
+    # @param line [String]
+    # @return     [Gff3Entry]
+    def parse(line)
+      fields = line.split("\t")
+      attributes = parse_attributes(fields[ATTRIBULE_COL_INDEX])
+      Gff3Entry.new(fields[0...ATTRIBULE_COL_INDEX], attributes)
+    end
+
+    private
+
+    # @param field [String, nil]
+    # @return      [Hash{ Symbol => String }]
+    def parse_attributes(field)
+      return {} unless field
+
+      field.split(';').map.to_h do |kv|
+        unless kv =~ /^([^=]+)=([^=]+)$/
+          warn "failed to parse attribute key-value: #{kv}"
+          exit 1
+        end
+
+        [$1.to_sym, $2]
+      end
+    end
+  end
+end
